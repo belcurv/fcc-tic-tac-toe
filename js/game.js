4b8e606e37d213b1794ed3023cbdd1d89be22af3
Jay Schwane <jrschwane@uwalumni.com>
2017-03-19 17:01:59 -0500

    Add or fix almost everything

--- js/game.js
+++ js/game.js
@@ -5,8 +5,207 @@ var Game = (function ($) {
 
     'use strict';
 
-    var DOM = {},
+    var DOM = {},        // populated by cacheDom()
 
+        gameState = {},  // populated by reset()
+        
+        victories = [    // possible victory square combos
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ],
+        
+        playerToken,     // populated by setPlayerToken()
+        computerToken;
+
+
+    /* ========================== private methods ========================== */
+
+    // cache DOM elements
+    function cacheDom() {
+        DOM.$game     = $('#game');
+        
+        // player token select
+        DOM.$pModal   = $(document.createElement('div'));
+        DOM.$pButtonX = $(document.createElement('button'));
+        DOM.$pButtonO = $(document.createElement('button'));
+        
+        // victory modal and reset button
+        DOM.$vModal   = $(document.createElement('div'));
+        DOM.$vButton  = $(document.createElement('button'));
+    }
+
+
+    // bind events
+    function bindEvents() {
+        DOM.$game.on('click', '.square', squareClickHandler);
+        DOM.$game.on('click', '.play-again', resetGame);
+        DOM.$game.on('click', '.p-token-btn', setPlayerToken);
+    }
+
+
+    // handle clicks to squares
+    function squareClickHandler(e) {
+        
+        if (gameState.gameOver) { return; }
+
+        // get clicked square's index
+        var sqIndex = (e.target.id).slice(2, 3);
+
+        // make sure it's player's turn & space is free
+        if (gameState.playerTurn && isAvailable(sqIndex)) {
+
+            // register move in gameState
+            gameState.spaces[sqIndex].val = playerToken;
+            
+            // set board text
+            $(e.target).text(playerToken);
+            
+            // increment turns
+            step();
+
+        }
+    }
+    
+    
+    // handle player token selection
+    function setPlayerToken(e) {
+        DOM.$pModal
+            .hide();
+        
+        if (e.target.id === 'playerX') {
+            playerToken   = 'X';
+            computerToken = 'O';
+        } else {
+            playerToken   = 'O';
+            computerToken = 'X';
+        }
+    }
+    
+    
+    // check if square avaialble
+    function isAvailable(sq) {
+        return gameState.spaces[sq].val === null;
+    }
+
+
+    // find remaining available moves
+    function availableMoves() {
+        return gameState.spaces.filter( s => s.val === null );
+    }
+
+
+    // computer's turn
+    function computerTurn(e) {
+        
+        if (gameState.gameOver) { return; }
+
+        var spaces = availableMoves(),
+            pick   = Math.floor(Math.random() * spaces.length),
+            square = $('#sq' + spaces[pick].ind);
+        
+        // set board text
+        square.text(computerToken);
+        
+        // register move in gameState
+        gameState.spaces[spaces[pick].ind].val = computerToken;
+        
+        // increment turns
+        step();
+    }
+
+
+    // check for a winner
+    function checkIfWinner() {
+        
+        ['X', 'O'].forEach(function (player) {
+            var occupiedSpaces = gameState.spaces
+                .filter( space => space.val === player )
+                .map(    space => space.ind );
+            
+            victories.forEach(function (v) {
+                var match = v.every( el => occupiedSpaces.indexOf(el) > -1 );
+                if (match) {
+                    // call victory with specified player
+                    victory(player);
+                }
+            });
+        });
+    }
+    
+    
+    // handle victory
+    function victory(player) {
+        gameState.gameOver = true;
+        showVictoryModal(player);
+    }
+    
+    
+    // build and show victory modal
+    function showVictoryModal(player) {
+        
+        DOM.$vButton
+            .addClass('play-again')
+            .attr('type', 'button')
+            .text('Play Again?');
+        
+        DOM.$vModal
+            .addClass('modal')
+            .appendTo(DOM.$game)
+            .html(`<p>Player ${player} wins!</p>`)
+            .append(DOM.$vButton)
+            .show();
+    }
+
+
+    // build the game board
+    function makeBoard() {
+
+        var i,
+            $board  = $(document.createElement('div')),
+            $square = $(document.createElement('div'));
+
+        $square.addClass('square');
+        $board.addClass('board');
+
+        // build board from squares
+        for (i = 0; i < 9; i += 1) {
+            $square.clone()
+                .attr('id', 'sq' + i)
+                .appendTo($board);
+        }
+
+        // append the board to the DOM
+        DOM.$game
+            .append($board);
+
+    }
+    
+    
+    // clear each square's text
+    function clearBoard() {
+        $('.square').each(function (ind, el) {
+            $(el).text('');
+        }); 
+    }
+
+    
+    // reset game
+    function resetGame() {
+        
+        // erase sqares' text
+        clearBoard();
+
+        // hide victory modal
+        DOM.$vModal
+            .hide();
+
+        // reset game state
         gameState = {
             spaces: [
                 { "val": null, "ind": 0 },
@@ -23,124 +222,58 @@ var Game = (function ($) {
             gameOver: false,
             gameResult: '',
             numberTurns: 0
-        },
-
-        victories = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [1, 4, 7],
-            [2, 5, 8],
-            [3, 6, 9],
-            [1, 5, 9],
-            [3, 5, 7]
-        ];
-
-
-    /* ========================== private methods ========================== */
-
-    // cache DOM elements
-    function cacheDom() {
-        DOM.$game   = $('#game');
-        DOM.$logger = $('#logger');
+        };
     }
-
-
-    // bind events
-    function bindEvents() {
-        DOM.$game.on('click', '.square', squareClickHandler);
-    }
-
-
-    // handle clicks to squares
-    function squareClickHandler(e) {
-
-        var sqIndex = (e.target.id).slice(2, 3);
-
-        if (gameState.playerTurn) {
-
-            // players's turn
-
-            // check if clicked square is available
-            if (gameState.spaces[sqIndex].val === null) {
-                gameState.spaces[sqIndex].val = 'X';
-                gameState.playerTurn = false;
-                gameState.numberTurns += 1;
-
-                $(e.target).text('X');
-                logger(gameState);
-
-            } else {
-                logger(`square ${++sqIndex} is not empty`);
-            }
-
-
-
-        }
-
-        setTimeout(function() {
-            computerTurn(e);
-        }, 1500);
-
-    }
-
-
-    // find remaining available moves
-    function availableMoves() {
-        return gameState.spaces.filter( s => s.val === null );
-    }
-
-
-    // computer's turn
-    function computerTurn(e) {
-
-        var spaces = availableMoves(),
-            pick   = Math.floor(Math.random() * spaces.length),
-            square = $('#sq' + spaces[pick].ind);
+    
+    
+    // advance game one turn at a time
+    function step() {
         
-        gameState.spaces[spaces[pick].ind].val = 'O';
-        square.text('O');
-        gameState.playerTurn = true;
+        checkIfWinner();
         gameState.numberTurns += 1;
-                
-        logger(gameState);
-    }
-
-
-    // log to screen
-    function logger(blah) {
-
-        DOM.$logger
-            .html(`<pre>${JSON.stringify(blah, null, 2)}</pre>`);
-
-    }
-
-
-    // build game board
-    function makeBoard() {
-
-        var i,
-            $board = $(document.createElement('div')),
-            $square = $(document.createElement('div'));
-
-        $square.addClass('square');
-        $board.addClass('board');
-
-        // build board from squares
-        for (i = 0; i < 9; i += 1) {
-            $square.clone()
-                .attr('id', 'sq' + i)
-                .appendTo($board);
+        
+        // check if it's the computer's turn
+        if (gameState.numberTurns % 2 !== 0) {
+            
+            // computer plays after a slight delay
+            setTimeout(function () {
+                computerTurn();
+            }, 500);
+            
         }
-
-        DOM.$game
-            .append($board);
-
+        
+        // toggle player's turn
+        gameState.playerTurn = !gameState.playerTurn;
+        
     }
-
-
-    // main renderer
-    function render() {}
+    
+    
+    // modal for player token select
+    function chooseSides() {
+        
+        var group = $(document.createElement('div'));
+        
+        DOM.$pButtonX
+            .addClass('p-token-btn')
+            .attr('id', 'playerX')
+            .attr('type', 'button')
+            .text('X')
+            .appendTo(group);
+        
+        DOM.$pButtonO
+            .addClass('p-token-btn')
+            .attr('id', 'playerO')
+            .attr('type', 'button')
+            .text('O')
+            .appendTo(group);
+        
+        DOM.$pModal
+            .addClass('modal')
+            .appendTo(DOM.$game)
+            .html(`<p>Choose Sides!</p>`)
+            .append(group)
+            .show();
+    }
 
 
     /* ========================== public methods =========================== */
@@ -151,6 +284,8 @@ var Game = (function ($) {
         cacheDom();
         bindEvents();
         makeBoard();
+        resetGame();
+        chooseSides();
 
     }
 
